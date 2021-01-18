0c5bd1549f5416436b925131d5aabf6b58aa4c19
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-18 08:15:39 -0500

    Adding version number in setup.py so code is not imported prematurely
    See https://github.com/fhightower-templates/python-project-template/issues/6

--- democritus_algorithms/__init__.py
+++ democritus_algorithms/__init__.py
@@ -2,5 +2,4 @@ __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
 __version__ = '2021.01.12'
 
-if __name__ == '__main__':
-    from .algorithms import *
+from .algorithms import *
